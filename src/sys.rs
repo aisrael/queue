45fb483154258256c7bea4dd232ae7ad4ca4abb4
Alistair A. Israel <aisrael@gmail.com>
2015-05-16 09:43:27 +0800

    Moved code around so logical grouping is clearer.

--- src/sys.rs
+++ src/sys.rs
@@ -1,20 +1,7 @@
 extern crate libc;
 
 use std::ffi::CString;
-
 use libc::mode_t;
-
-mod raw {
-
-	use libc::c_char;
-	use libc::c_int;
-    use libc::mode_t;
-
-	extern "system" {
-	    pub fn mkfifo(path: *const c_char, mode: mode_t) -> c_int;
-	    pub fn unlink(path: *const c_char) -> c_int;
-	}
-}
 
 pub fn mkfifo(path: &str, mode: u16) -> i32 {
     let c_path = CString::new(path).unwrap();
@@ -30,5 +17,17 @@ pub fn unlink(path: &str) -> i32 {
     let p_path = c_path.as_ptr();
     unsafe {
         return raw::unlink(p_path);
+    }
+}
+
+mod raw {
+
+    use libc::c_char;
+    use libc::c_int;
+    use libc::mode_t;
+
+    extern "system" {
+        pub fn mkfifo(path: *const c_char, mode: mode_t) -> c_int;
+        pub fn unlink(path: *const c_char) -> c_int;
     }
 }
